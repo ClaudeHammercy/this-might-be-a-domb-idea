fb34b8c3b512718310860a95145f64bbf29021f0
Rashad Gant <83968751+rashadgant@users.noreply.github.com>
2021-10-31 21:58:41 -0400

    complete

---
+++ main.js
@@ -0,0 +1,24 @@
+const paragraph = document.querySelector(innerText);
+paragraph.style.fontColor = 'lightblue';
+
+const h1 = document.querySelector('h1');
+h1.style.fontSize = '48px';
+
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+const numberThirteen = document.querySelector('#item-13');
+numberThirteen.style.opacity = 0.5;
+
+const numberThree = document.querySelector('#item-3');
+numberThree.innerText = 'Hi!';
+
+const image = document.querySelector('img');
+image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+image.style.height = '300px';
+
+const cactus = document.querySelector('.cactus');
+cactus.style.height = '300px';
+
+const itemSixteen = document.querySelector('#item-16');
+itemSixteen.className = 'item';
+itemSixteen.innerText = 'Won\'t get fooled again!';
